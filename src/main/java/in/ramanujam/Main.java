e66f0b213876ac78e937ba1954f6425a16f589fe
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2016-06-29 22:05:00 +0300

    added elastic search container

--- src/main/java/in/ramanujam/Main.java
+++ src/main/java/in/ramanujam/Main.java
@@ -15,18 +15,29 @@ public class Main {
         DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
 
         ExposedPort tcp6379 = ExposedPort.tcp(6379);
-        Ports portBindings = new Ports();
-        portBindings.bind(tcp6379, new Ports.Binding("localhost", "7777"));
+        Ports redisPortBinding = new Ports();
+        redisPortBinding.bind(tcp6379, new Ports.Binding("localhost", "7777"));
 
-        CreateContainerResponse container = dockerClient.createContainerCmd("redis") // TODO: check that we are not duplicating containers
+        ExposedPort tcp9200 = ExposedPort.tcp(9200);
+        Ports ESPortBinding = new Ports();
+        ESPortBinding.bind(tcp9200, new Ports.Binding("localhost", "5555"));
+
+        CreateContainerResponse redisContainer = dockerClient.createContainerCmd("redis") // TODO: check that we are not duplicating containers
                 .withExposedPorts(tcp6379) // TODO: be sure that the port 7777 will be available for the next run of main method
-                .withPortBindings(portBindings)
+                .withPortBindings(redisPortBinding)
                 .exec();
-        dockerClient.startContainerCmd(container.getId()).exec();
+
+        CreateContainerResponse ESContainer = dockerClient.createContainerCmd("elasticsearch")
+                .withExposedPorts(tcp9200)
+                .withPortBindings(ESPortBinding)
+                .exec();
+
+        dockerClient.startContainerCmd(redisContainer.getId()).exec();
+        dockerClient.startContainerCmd(ESContainer.getId()).exec();
 
         SpringApplication.run(RedisFillerMain.class, args);
 
-        dockerClient.waitContainerCmd(container.getId()).exec( new WaitContainerResultCallback()).awaitStatusCode(); // TODO: use waitContainerCmd!
+        dockerClient.waitContainerCmd(redisContainer.getId()).exec( new WaitContainerResultCallback()).awaitStatusCode(); // TODO: use waitContainerCmd!
 //        dockerClient.stopContainerCmd(container.getId()).exec();
     }
 }
