eb1262f1f7a6440cf3df3ede4d6a05bc2fba19e0
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2016-07-02 09:44:25 +0300

    simple "get all" redis retriever

--- src/main/java/in/ramanujam/service/retriever/RedisRetriever.java
+++ src/main/java/in/ramanujam/service/retriever/RedisRetriever.java
@@ -1,9 +1,12 @@
 package in.ramanujam.service.retriever;
 
 import in.ramanujam.model.RedisRecord;
-import in.ramanujam.service.TestUtil;
+import in.ramanujam.properties.RedisProperties;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,6 +18,12 @@ public class RedisRetriever
 {
   public static List<RedisRecord> retrieveAllRecords()
   {
-    return TestUtil.generateRedisRecords();
+    RedisProperties redisProperties = new RedisProperties();
+    Jedis jedis = new Jedis( redisProperties.getRedisContainerHost(), redisProperties.getRedisContainerExternalPort());
+
+    Map<String, String> redisRecordMap = jedis.hgetAll( redisProperties.getRedisHashsetName() );
+    return redisRecordMap.entrySet().stream()
+            .map( entry -> new RedisRecord( Integer.parseInt( entry.getKey() ), entry.getValue() ) )
+            .collect( Collectors.toList() );
   }
 }
