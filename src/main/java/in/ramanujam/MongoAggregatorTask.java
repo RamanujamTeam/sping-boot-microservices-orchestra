840439850e595c8970116e356931433a6332315f
Denys Konakhevych <denys.konakhevich@themidnightcoders.com>
2016-07-05 10:44:50 +0300

    removed comments

--- src/main/java/in/ramanujam/MongoAggregatorTask.java
+++ src/main/java/in/ramanujam/MongoAggregatorTask.java
@@ -42,10 +42,6 @@ public class MongoAggregatorTask
             List<MongoDBRecord> records = retrieveMongoDBRecords();
             MongoAggregator.aggregate( collection, records );
         }
-//        catch( UnknownHostException e )
-//        {
-//            throw new RuntimeException( e );
-//        }
         finally
         {
             if( mongo != null )
--- src/main/java/in/ramanujam/service/MongoAggregatorMain.java
+++ src/main/java/in/ramanujam/service/MongoAggregatorMain.java
@@ -29,10 +29,6 @@ public class MongoAggregatorMain
       List<MongoDBRecord> records = TestUtil.generateMongoRecords();
       MongoAggregator.aggregate( collection, records );
     }
-//    catch( UnknownHostException e )
-//    {
-//      throw new RuntimeException( e );
-//    }
     finally
     {
       if( mongo != null )
