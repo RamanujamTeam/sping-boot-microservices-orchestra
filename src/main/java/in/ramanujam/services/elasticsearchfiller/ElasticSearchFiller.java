f5c6c4e3683a0f01e0ed5ec9161493522dde3ed5
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2016-07-10 23:43:34 +0300

    Added logging, spring DI, refactored filler

--- src/main/java/in/ramanujam/services/elasticsearchfiller/ElasticSearchFiller.java
+++ src/main/java/in/ramanujam/services/elasticsearchfiller/ElasticSearchFiller.java
@@ -7,25 +7,36 @@ import in.ramanujam.common.properties.ElasticSearchProperties;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Denys Konakhevych
- * Date: 08.07.2016
- * Time: 18:26
- */
+@Service
 public class ElasticSearchFiller
 {
-  private static ElasticSearchFiller instance = new ElasticSearchFiller();
-  private Client client;
+  private static final Logger log = LoggerFactory.getLogger( ElasticSearchFiller.class );
+  private final Client client;
   private final String index;
   private final String type;
+
+  //debug
   private static int count = 0;
 
-  private ElasticSearchFiller()
+  @Value("elastic-data.json")
+  private Resource elasticSearchFile;
+
+  public ElasticSearchFiller()
   {
     ElasticSearchProperties elasticSearchProperties = new ElasticSearchProperties();
     index = elasticSearchProperties.getElasticsearchIndexName();
@@ -33,26 +44,37 @@ public class ElasticSearchFiller
     try
     {
       client = TransportClient.builder().build()
-              .addTransportAddress( new InetSocketTransportAddress(
-                      InetAddress.getByName( elasticSearchProperties.getElasticsearchContainerHost() ),
-                      elasticSearchProperties.getElasticsearchContainerExternalPort() ) );
+          .addTransportAddress( new InetSocketTransportAddress(
+              InetAddress.getByName( elasticSearchProperties.getElasticsearchContainerHost() ),
+              elasticSearchProperties.getElasticsearchContainerExternalPort() ) );
     }
-    catch( UnknownHostException e )
+    catch ( UnknownHostException e )
     {
       throw new RuntimeException( e );
     }
-  }
-
-  public static ElasticSearchFiller getInstance()
-  {
-    return instance;
   }
 
   public void addMinerRecord( MinerRecord minerRecord ) throws JsonProcessingException
   {
     ObjectMapper mapper = new ObjectMapper();
     client.prepareIndex( index, type, minerRecord.getId().toString() )
-            .setSource( mapper.writeValueAsBytes( minerRecord ) ).get();
-    System.out.println( "ElasticSearchFiller :: Id = " + minerRecord.getId() + " count = " + ++count );
+        .setSource( mapper.writeValueAsBytes( minerRecord ) ).get();
+    log.trace( "ElasticSearchFiller :: Id = " + minerRecord.getId() + " count = " + ++count );
+  }
+
+  public void fillItems( int offset, int limit )
+  {
+    //stream should be here
+    try
+    {
+      InputStream file = elasticSearchFile.getInputStream();
+      //stream should be here
+      //...
+      log.info( offset + " " + limit);
+    }
+    catch ( IOException e )
+    {
+      throw new RuntimeException( e );
+    }
   }
 }
--- src/main/java/in/ramanujam/services/elasticsearchfiller/ElasticSearchFillerScheduledTask.java
+++ src/main/java/in/ramanujam/services/elasticsearchfiller/ElasticSearchFillerScheduledTask.java
@@ -2,6 +2,7 @@ package in.ramanujam.services.elasticsearchfiller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.ramanujam.common.model.MinerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,21 +16,18 @@ import java.util.List;
 @Component
 public class ElasticSearchFillerScheduledTask
 {
-    @Value("elastic-data.json")
-    private Resource elasticSearchFile;
 
-    private int curPos = 1;
+    @Autowired
+    ElasticSearchFiller filler;
+
+    public static final int PAGE_SIZE = 10;
+    private int curPos = 0;
+
     // TODO: how can we stop it from running after all records are persisted?
     @Scheduled(fixedDelay = 1000) // TODO: 30 secs
     public void runWithDelay() throws IOException
     {
-        ObjectMapper mapper = new ObjectMapper();
-        MinerRecord[] esRecords = mapper.readValue( elasticSearchFile.getFile(), MinerRecord[].class ); // TODO: can we improve this logic?
-        List<MinerRecord> minerRecords = new ArrayList<>( Arrays.asList( esRecords ) );
-        int lastIndex = Math.min( curPos + 100, minerRecords.size() + 1 );
-        while( curPos < lastIndex )
-        {
-            ElasticSearchFiller.getInstance().addMinerRecord( minerRecords.get( curPos++ - 1 ) );
-        }
+        filler.fillItems( curPos, PAGE_SIZE );
+        curPos += PAGE_SIZE;
     }
 }
