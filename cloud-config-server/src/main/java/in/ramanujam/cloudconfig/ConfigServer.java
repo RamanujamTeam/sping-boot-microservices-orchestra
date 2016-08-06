d744d06603a15ce29f62b9275ac871a5f705e75d
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2016-08-06 17:25:04 +0300

    Deleted all properties files

--- cloud-config-server/src/main/java/in/ramanujam/cloudconfig/ConfigServer.java
+++ cloud-config-server/src/main/java/in/ramanujam/cloudconfig/ConfigServer.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer {
+public class ConfigServer {  // Note that configuration file with repository URI should be named application.yml - it won't work otherwise
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
